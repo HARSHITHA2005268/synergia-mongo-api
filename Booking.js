c33ef96786d19fbf7b6cc7be1e11b642d09f72ea
Harshitha <kulalharshitha2005@gmail.com>
2026-03-28 00:17:12 +0530

    Added gitignore and removed sensitive files

---
+++ Booking.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  event: {
+    type: String,
+    required: true,
+  },
+  ticketType: {
+    type: String,
+    default: "General",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
